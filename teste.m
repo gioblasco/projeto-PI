12e3c9b3a5bec307a4c27e93996c00dc690d0fbb
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-07-19 20:15:03 -0300

    comparacoes

--- teste.m
+++ teste.m
@@ -1,6 +1,7 @@
 clear all, close all, clc;
 
 [foto] = imread("placa-para.jpg");
+template = imread("./Temp_placa/R_1.jpg");
 figure, imshow(foto);
 
 redplane = foto(:, :, 1);
@@ -19,17 +20,23 @@ justRed = imfill(justRed, 'holes');
 justRed = imclose(justRed, strel("disk", 4, 0));
 justRed = bwareaopen(justRed, 30);
 
-figure, imshow(justRed);
-
 %% recupera objetos que podem ser placas
 conec = bwconncomp(justRed);
 regioes = regionprops(conec, "basic");
 
 figure, imshow(justRed);
-figure, imshow(foto);
 for i = 1:rows(regioes)
   fronteira = regioes(i).BoundingBox;
+  placa = im2bw(rgb2gray(foto));
   rectangle('Position', fronteira, 'EdgeColor', 'blue', 'LineWidth', 3);
-  placa = imcrop(foto, [fronteira(1) fronteira(2) fronteira(3) fronteira(4)]);
-  figure, imshow(placa);
+  placa = imcrop(foto, round(fronteira));
+  %figure, subplot(1,2,1), imshow(placa);
+  template_resized = im2bw(rgb2gray(imresize(template, size(placa))));
+  correlacao(i) = corr2(placa, template_resized);
+  %subplot(1,2,2), imshow(template_resized), title(num2str(correlacao));
 endfor
+
+[maior_corr, melhor_regiao] = max(correlacao);
+fronteira = regioes(melhor_regiao).BoundingBox;
+figure, imshow(foto);
+rectangle('Position', fronteira, 'EdgeColor', 'blue', 'LineWidth', 3);
