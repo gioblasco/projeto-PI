449439d3f8019bc0688dd48b022dde3e940462b6
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-07-20 22:30:39 -0300

    resolvendo conflito

--- teste.m
+++ teste.m
@@ -1,12 +1,7 @@
 clear all, close all, clc;
 
-<<<<<<< HEAD
-[foto] = imread("placa-transito.jpg");
-template = imread("./Temp_placa/R_3.jpg");
-=======
-[foto] = imread("pare-rua3.jpg");
-template = imread("./Temp_placa/teste.jpg");
->>>>>>> 19523c6187b2533e61bf396886417c1d2ac1def6
+[foto] = imread("pare.jpg");
+template = imread("./Temp_placa/R_1.jpg");
 figure, imshow(foto);
 
 redplane = foto(:, :, 1);
@@ -30,7 +25,6 @@ conec = bwconncomp(justRed);
 regioes = regionprops(conec, "basic", "Perimeter");
 figure, imshow(foto);
 
-<<<<<<< HEAD
 for i = 1:rows(regioes)
     fronteira = regioes(i).BoundingBox;
     placa = imcrop(foto, ajusta_bbox(fronteira, size(foto)));
@@ -48,18 +42,6 @@ for i = 1:rows(regioes)
     
     figure, subplot(1,2,1), imshow(placa);
     subplot(1,2,2), imshow(template_resized);
-=======
-figure, imshow(foto);
-for i = 1:rows(regioes)
-  fronteira = regioes(i).BoundingBox;  
-  placa = imcrop(foto, floor(fronteira));
-  placa = im2bw(rgb2gray(placa));
-  rectangle('Position', fronteira, 'EdgeColor', 'blue', 'LineWidth', 3);
-  %figure, subplot(1,2,1), imshow(placa);
-  template_resized = im2bw(rgb2gray(imresize(template, size(placa))));
-  correlacao(i) = corr2(placa, template_resized);
-  %subplot(1,2,2), imshow(template_resized), title(num2str(correlacao));
->>>>>>> 19523c6187b2533e61bf396886417c1d2ac1def6
 endfor
 
 [maior_corr, melhor_regiao] = max(correlacao)
