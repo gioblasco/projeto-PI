10e4071432407d067c9c751301d096660a1c9b73
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-08-09 13:35:04 -0300

    adicionando funcionamento para várias placas

--- mult.m
+++ mult.m
@@ -1,14 +1,21 @@
 clear all, close all, clc;
 
-foto = imread("./ExemplosCertos/buzina2.jpg");
+foto = imread("./ExemplosCertos/placas2.jpg");
 
 figure, imshow(foto);
 
-ident = preprocessing(foto);
+original = foto;
 
 %% pega arquivos da pasta
 folder = './RedTemplates/*.jpg';
 files = dir(folder);
+
+count = 0;
+notfound = 0;
+
+while notfound == 0
+  
+ident = preprocessing(foto);
 
 fronteira = [];
 maior_corr = [];
@@ -29,14 +36,25 @@ end
 [corr, indcorr] = max(maior_corr);
 
 % se a correlacao n for negativa e houver fronteira (ou seja, algum  objeto foi identificado)
-if(corr > 0 && !isempty(fronteira))
+if(corr > 0.1 && !isempty(fronteira))
   proibido = checkAllowed(foto, fronteira(indcorr,:));
-  figure, imshow(foto);
+  figure, imshow(original);
   nome = mapname(files(indcorr).name, proibido);
+  title(nome);
+  count = count + 1;
+  corr
   rectangle('Position', fronteira(indcorr, :), 'EdgeColor', 'blue', 'LineWidth', 3);
+  ajustada = ajusta_bbox(fronteira(indcorr, :), size(foto));
+  x2 = ajustada(1) + ajustada(3);
+  y2 = ajustada(2) + ajustada(4);
+  foto(ajustada(2):y2, ajustada(1):x2, :) = 0;
 else % se nenhuma possivel placa foi identificada
-  figure, imshow(foto);
-  nome = 'PLACA NAO IDENTIFICADA';
+  if count == 0
+    figure, imshow(original);
+    nome = 'PLACA NAO IDENTIFICADA';
+    title(nome);
+   endif
+   notfound = 1;
 endif
 
-title(nome);
+endwhile
