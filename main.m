63f1048d10c2ceac811ab17a07216df6c028c03c
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-08-10 21:02:20 -0300

    commit final

--- main.m
+++ main.m
@@ -1,9 +1,11 @@
+% esta main eh utilizada para identificação de imagens com uma placa apenas
 clear all, close all, clc;
 
-foto = imread("./Exemplos/ciclovia2.jpg");
+foto = imread("./ExemplosCertos/placas3.jpg");
 
 figure, imshow(foto);
 
+% pega imagem pre-processada
 ident = preprocessing(foto);
 
 %% pega arquivos da pasta
@@ -21,6 +23,7 @@ maximo = max(compacidades(range));
 
 % faz match com cada template de placa
 for i = 1:length(files)
+	% leitura do template
 	template = imread(strcat('./RedTemplates/', files(i).name));
 	[fronteira(i, :), maior_corr(i, 1)] = match(foto, template, regioes, compacidades, maximo);
 end
@@ -30,10 +33,17 @@ end
 
 % se a correlacao n for negativa e houver fronteira (ou seja, algum  objeto foi identificado)
 if(corr > 0 && !isempty(fronteira))
+  % checa se é proibida
   proibido = checkAllowed(foto, fronteira(indcorr,:));
+  
   figure, imshow(foto);
+  
+  % determina nome conforme o nome do arquivo
   nome = mapname(files(indcorr).name, proibido);
+  
+  % desenha retangulo em volta da placa
   rectangle('Position', fronteira(indcorr, :), 'EdgeColor', 'blue', 'LineWidth', 3);
+  
 else % se nenhuma possivel placa foi identificada
   figure, imshow(foto);
   nome = 'PLACA NAO IDENTIFICADA';
--- mult.m
+++ mult.m
@@ -1,6 +1,7 @@
+% esta main eh utilizada para identificação de imagens com várias placas
 clear all, close all, clc;
 
-foto = imread("./ExemplosCertos/placas2.jpg");
+foto = imread("./ExemplosCertos/placas3.jpg");
 
 figure, imshow(foto);
 
@@ -10,11 +11,15 @@ original = foto;
 folder = './RedTemplates/*.jpg';
 files = dir(folder);
 
+% conta o numero de placas identificadas
 count = 0;
+
 notfound = 0;
 
+% enquanto houver alguma placa para ser identificada
 while notfound == 0
-  
+
+% pega imagem pre-processada
 ident = preprocessing(foto);
 
 fronteira = [];
@@ -28,6 +33,7 @@ maximo = max(compacidades(range));
 
 % faz match com cada template de placa
 for i = 1:length(files)
+	% leitura do template
 	template = imread(strcat('./RedTemplates/', files(i).name));
 	[fronteira(i, :), maior_corr(i, 1)] = match(foto, template, regioes, compacidades, maximo);
 end
@@ -37,22 +43,33 @@ end
 
 % se a correlacao n for negativa e houver fronteira (ou seja, algum  objeto foi identificado)
 if(corr > 0.1 && !isempty(fronteira))
+  % checa se é proibida
   proibido = checkAllowed(foto, fronteira(indcorr,:));
   figure, imshow(original);
+  
+  % determina nome conforme o nome do arquivo
   nome = mapname(files(indcorr).name, proibido);
   title(nome);
   count = count + 1;
+  
+  % desenha retangulo em volta da placa
   rectangle('Position', fronteira(indcorr, :), 'EdgeColor', 'blue', 'LineWidth', 3);
+  
+  % colore espaco da placa identificada para nao ser identificada novamente
   ajustada = ajusta_bbox(fronteira(indcorr, :), size(foto));
   x2 = ajustada(1) + ajustada(3);
   y2 = ajustada(2) + ajustada(4);
   foto(ajustada(2):y2, ajustada(1):x2, :) = 0;
-else % se nenhuma possivel placa foi identificada
-  if count == 0
+else 
+
+   % se nenhuma possivel placa foi identificada
+   if count == 0
     figure, imshow(original);
     nome = 'PLACA NAO IDENTIFICADA';
     title(nome);
    endif
+   
+   % nao achou mais placas
    notfound = 1;
 endif
 
